224293c0e26a3a39ccf3848318bce95ddcd66421
Jeffrey <1115378579@qq.com>
2023-02-24 12:14:00 +0800

    chaore: init

---
+++ components/Header.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { name as webName } from '../package.json'
+
+</script>
+<template>
+  <div flex justify-between>
+    <h1 class="text-4xl font-semibold sm:text-5xl u-text-gray-900">{{ webName }}</h1>
+    <div></div>
+    <Links />
+  </div>
+</template>
--- layouts/home.vue
+++ layouts/home.vue
@@ -1,7 +1,7 @@
 <template>
-  <main class="py-20 px-10 text-center">
+  <main class="p-4">
+    <Header />
     <slot />
-    <Footer />
     <div class="mt-5 mx-auto text-center opacity-25 text-sm">
       [Home Layout]
     </div>
